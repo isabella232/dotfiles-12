6ee1a6129fb782106119fee569ad4725fc9510ea
Jonathan Amiez <jonathan.amiez@gmail.com>
2012-12-13 19:43:17 +0100

    move debian plugin to apt lib

---
+++ files/zsh/lib/apt.zsh
@@ -0,0 +1,61 @@
+# Aliases ###################################################################
+alias acs="apt-cache search"
+alias afs='apt-file search --regexp'
+
+alias apti='sudo apt-get install'
+alias aptac='sudo apt-get autoclean'
+alias aptd='sudo apt-get update'
+alias aptg='sudo apt-get upgrade'
+alias aptdg='sudo apt-get update && sudo apt-get dist-upgrade'
+alias aptr='sudo apt-get purge'
+
+# create a simple script that can be used to 'duplicate' a system
+apt-copy() {
+    print '#!/bin/sh'"\n" > apt-copy.sh
+
+    cmd='apt-get install'
+
+    for p in ${(f)"$(aptitude search -F "%p" --disable-columns \~i)"}; {
+        cmd="${cmd} ${p}"
+    }
+
+    print $cmd "\n" >> apt-copy.sh
+
+    chmod +x apt-copy.sh
+}
+
+# Prints apt history
+# Usage:
+#   apt-history install
+#   apt-history upgrade
+#   apt-history remove
+#   apt-history rollback
+#   apt-history list
+# Based On: http://linuxcommando.blogspot.com/2008/08/how-to-show-apt-log-history.html
+apt-history () {
+  case "$1" in
+    install)
+      zgrep --no-filename 'install ' $(ls -rt /var/log/dpkg*)
+      ;;
+    upgrade|remove)
+      zgrep --no-filename $1 $(ls -rt /var/log/dpkg*)
+      ;;
+    rollback)
+      zgrep --no-filename upgrade $(ls -rt /var/log/dpkg*) | \
+        grep "$2" -A10000000 | \
+        grep "$3" -B10000000 | \
+        awk '{print $4"="$5}'
+      ;;
+    list)
+      zcat $(ls -rt /var/log/dpkg*)
+      ;;
+    *)
+      echo "Parameters:"
+      echo " install - Lists all packages that have been installed."
+      echo " upgrade - Lists all packages that have been upgraded."
+      echo " remove - Lists all packages that have been removed."
+      echo " rollback - Lists rollback information."
+      echo " list - Lists all contains of dpkg logs."
+      ;;
+  esac
+}
