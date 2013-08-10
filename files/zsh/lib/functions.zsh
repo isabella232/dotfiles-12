28139fa207db0e355f745ec24cd7487d044ef0c6
Jonathan Amiez <jonathan.amiez@gmail.com>
2013-08-10 13:38:50 +0200

    add gpg-agent support to zsh setup

--- files/zsh/lib/functions.zsh
+++ files/zsh/lib/functions.zsh
@@ -2,18 +2,6 @@ function zsh_stats() {
   history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
 }
 
-
-function load_ssh_keys() {
-  if [ $SSH_AUTH_SOCK ]; then
-    for key in $(find $HOME/.ssh -name '*_rsa'); do
-      if [ `/usr/bin/ssh-add -l | grep $key | wc -l` -ne 1 ]; then
-        /usr/bin/ssh-add $key
-      fi
-    done
-  fi
-}
-
-
 function encode64() {
   echo -n $1 | base64
 }
---
+++ files/zsh/lib/gpg-agent.zsh
@@ -0,0 +1,34 @@
+local GPG_ENV=$HOME/.gnupg/gpg-agent.env
+
+function start_gpg_agent {
+  /usr/bin/env gpg-agent --daemon --enable-ssh-support --write-env-file ${GPG_ENV} > /dev/null
+  chmod 600 ${GPG_ENV}
+  . ${GPG_ENV} > /dev/null
+}
+
+function setup_gpg_agent() {
+  if [ -f "${GPG_ENV}" ]; then
+    . ${GPG_ENV} > /dev/null
+    ps -ef | grep ${SSH_AGENT_PID} | grep gpg-agent > /dev/null
+    [ $? -ne 0 ] && start_gpg_agent
+  else
+    start_gpg_agent
+  fi
+
+  GPG_TTY=$(tty)
+
+  export GPG_AGENT_INFO
+  export SSH_AUTH_SOCK
+  export SSH_AGENT_PID
+  export GPG_TTY
+}
+
+function load_ssh_keys() {
+  if [ $SSH_AUTH_SOCK ]; then
+    for key in $(find $HOME/.ssh -name '*_rsa'); do
+      if [ $(/usr/bin/ssh-add -l | grep $key | wc -l) -ne 1 ]; then
+        /usr/bin/ssh-add $key
+      fi
+    done
+  fi
+}
