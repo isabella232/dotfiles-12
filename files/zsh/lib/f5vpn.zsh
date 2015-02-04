e850541a9bbc3fba467435c2c4cd5096063d9a99
Jonathan Amiez <jonathan.amiez@gmail.com>
2015-02-04 23:55:00 +0100

    update dotfiles

--- files/zsh/lib/f5vpn.zsh
+++ files/zsh/lib/f5vpn.zsh
@@ -1,8 +1,13 @@
+F5DIR=$HOME/.F5Networks
 if [ ! -f /usr/local/lib/F5Networks/SSLVPN/var/run/svpn.pid ]; then
-  if [ -f $HOME/.F5Networks/credentials.gpg ]; then
-    echo "Starting F5 VPN"
-    creds=$(gpg2 -dq $HOME/.F5Networks/credentials.gpg)
-    f=("${(s/:/)creds}")
-    f5fpc --start --host $f[1] --user $f[2] --password $f[3] > /dev/null
+  if [ -f $F5DIR/credentials.gpg ]; then
+    if [ ! -f $F5DIR/f5fpc.lock ]; then
+      touch $F5DIR/f5fpc.lock
+      echo "Starting F5 VPN"
+      creds=$(gpg2 -dq $HOME/.F5Networks/credentials.gpg)
+      f=("${(s/:/)creds}")
+      f5fpc --start --host $f[1] --user $f[2] --password $f[3] > /dev/null
+      rm -f $F5DIR/f5fpc.lock
+    fi
   fi
 fi
