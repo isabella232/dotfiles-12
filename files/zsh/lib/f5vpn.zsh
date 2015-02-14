d328e534a5a372718990007ffd2d47878b7c4a6f
Jonathan Amiez <jonathan.amiez@gmail.com>
2015-02-14 13:09:30 +0100

    update tmux & vpn function

--- files/zsh/lib/f5vpn.zsh
+++ files/zsh/lib/f5vpn.zsh
@@ -1,13 +1,22 @@
-F5DIR=$HOME/.F5Networks
-if [ ! -f /usr/local/lib/F5Networks/SSLVPN/var/run/svpn.pid ]; then
-  if [ -f $F5DIR/credentials.gpg ]; then
-    if [ ! -f $F5DIR/f5fpc.lock ]; then
-      touch $F5DIR/f5fpc.lock
-      echo "Starting F5 VPN"
-      creds=$(gpg2 -dq $HOME/.F5Networks/credentials.gpg)
-      f=("${(s/:/)creds}")
-      f5fpc --start --host $f[1] --user $f[2] --password $f[3] > /dev/null
-      rm -f $F5DIR/f5fpc.lock
+function f5vpn() {
+  F5DIR=$HOME/.F5Networks
+  F5PID=/usr/local/lib/F5Networks/SSLVPN/var/run/svpn.pid
+  if [ -f $F5PID ]; then
+    if [ -e /proc/$(cat $F5PID) ]; then
+      echo "F5 VPN already running"
+    else
+      start_f5vpn
     fi
+  else
+    start_f5vpn
   fi
-fi
+}
+
+function start_f5vpn() {
+  if [ -f $F5DIR/credentials.gpg ]; then
+    echo "Starting F5 VPN"
+    creds=$(gpg2 -dq $HOME/.F5Networks/credentials.gpg)
+    f=("${(s/:/)creds}")
+    f5fpc --start --host $f[1] --user $f[2] --password $f[3] > /dev/null
+  fi
+}
